20dc61a067a788d9e097003f0b2e53bf46c438e0
imserimkim <tpfla22@naver.com>
2023-03-09 16:34:34 +0900

    20230309

---
+++ js/main.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+  $(".customer-logos").slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1500,
+    arrows: false,
+    dots: false,
+    pauseOnHover: false,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 520,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+    ],
+  });
+});
