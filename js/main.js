ca0986544121a0ef0fd3785b881f3b786948e137
imserimkim <tpfla22@naver.com>
2023-03-16 18:18:13 +0900

    20230316

--- js/main.js
+++ js/main.js
@@ -1,3 +1,31 @@
+fetch("./js/index.json")
+  .then((response) => response.json())
+  .then((bag) => callback(bag));
+
+function callback(bag) {
+  const elDot = document.querySelectorAll(".dot"),
+    slider = document.querySelector(".customer-logos");
+  console.log(slider);
+  console.log(elDot);
+  let bags = bag.data;
+  console.log(bags.chanel.images);
+
+  elDot.forEach((dot, i) => {
+    dot.addEventListener("click", () => {
+      const ttt = bags.chanel.images;
+
+      console.log("hhhh");
+      slider.innerHTML = "";
+      console.log(ttt);
+      ttt[0].forEach((o, k) => {
+        slider.innerHTML += `<div class="slide">
+        <img src="${o}" />
+      </div>`;
+      });
+    });
+  });
+}
+
 $(document).ready(function () {
   $(".customer-logos").slick({
     slidesToShow: 3,
